b3216ba7c788bc4120a8255287a1ccdfd88015eb
0xf499d5 <f499d5@gmail.com>
2025-09-07 19:02:43 +0800

    重建适配矩池云A16 GPU容器的Conda环境

--- run_ddp_tutorial.sh
+++ run_ddp_tutorial.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0,1 python ddp_tutorial.py
+
+# source /root/miniconda3/bin/activate myconda
+# CUDA_VISIBLE_DEVICES=0,1 python ddp_tutorial.py
+# conda deactivate
+
+CUDA_VISIBLE_DEVICES=0,1 conda run -n myconda python ddp_tutorial.py
